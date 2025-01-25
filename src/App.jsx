774bd7b8eb007b136a74ee6bf40c27bfe1cde305
kaviraj-m <kaviraj9042@gmail.com>
2025-01-25 14:45:46 +0530

    scroll update

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,19 @@
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Navbar from './components/Navbar';
-import Footer from './components/Footer';
-import Home from './pages/Homepage';
-import BuildYourPCPage from './pages/BuildYourPCPage';
-import PreBuiltPCsPage from './pages/PreBuiltPCsPage';
-import AboutUs from './pages/AboutUs';
-import ContactUsPage from './pages/ContactUsPage';
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+import Home from "./pages/Homepage";
+import BuildYourPCPage from "./pages/BuildYourPCPage";
+import PreBuiltPCsPage from "./pages/PreBuiltPCsPage";
+import AboutUs from "./pages/AboutUs";
+import ContactUsPage from "./pages/ContactUsPage";
+import ScrollToTop from "./components/ScrollToTop";
 
 const App = () => {
   return (
     <Router>
-      <div style={{ backgroundColor: '#000', minHeight: '100vh' }}>
+      <ScrollToTop />
+      <div style={{ backgroundColor: "#000", minHeight: "100vh" }}>
         <Navbar />
         <Routes>
           <Route path="/" element={<Home />} />
